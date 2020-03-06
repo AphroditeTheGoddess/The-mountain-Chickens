799ead4a9cf55770345a8ee37502a65bed397874
ThisWasSparta <floris245@gmail.com>
2020-03-06 14:57:41 +0100

    Initial push
    Leeg framework voor de applicatie met 1 voorbeeld class

---
+++ Game1.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MoneyMakerTutorial
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        //TODO: Add multiple invaders here
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.IsFullScreen = true;
+            graphics.ApplyChanges();
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // Pass often referenced variables to Global
+            Global.GraphicsDevice = GraphicsDevice;            
+            Global.content = Content;
+
+            // Create and Initialize game objects
+
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Global.spriteBatch = spriteBatch;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Pass keyboard state to Global so we can use it everywhere
+            Global.keys = Keyboard.GetState();
+            // Update the game objects
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {            
+            spriteBatch.Begin();
+            // Draw the background (and clear the screen)
+
+            // Draw the game objects
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
